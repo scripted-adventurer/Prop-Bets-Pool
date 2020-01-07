0abc71cb8d047bc8e19097bc2a26742cdc8b25cf
Scripted Adventurer <scripted.adventurer@fake.com>
2020-01-07 17:32:31 -0500

    Updating settings.

--- prop_bets/settings.py
+++ prop_bets/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qbp7111i^=r2d49hmsbqonrg4_vvgb=cj^#255k8gz2@(j7_z='
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
